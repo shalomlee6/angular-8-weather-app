20f498a00bf85f413b4a331774a9d664312e0dcb
shalom lee pinchas <shalomlee6@gmail.com>
2019-10-17 16:11:22 +0300

    api key set

--- src/app/config/cofig.api.ts
+++ src/app/config/cofig.api.ts
@@ -1,5 +1,5 @@
 export const api = {
-  api_key: 'scwAIuIQo2DNGCityPDCH9gJ16YwMCbd',
+  api_key: 'RjMHcPHegA3cSZxEOK0G7aYaIPlv0d15',
   auto_complete_url: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
   deafult_weather_url: 'http://dataservice.accuweather.com/currentconditions/v1/',
   five_dayes_forecast_url: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',

467083909422dbfcf5ee1f1793392491e1f8c737
shalom lee pinchas <shalomlee6@gmail.com>
2019-10-23 02:10:59 +0300

    fix http calls to https
    adding s to the http://

--- src/app/config/cofig.api.ts
+++ src/app/config/cofig.api.ts
@@ -1,7 +1,7 @@
 export const api = {
   api_key: 'RjMHcPHegA3cSZxEOK0G7aYaIPlv0d15',
-  auto_complete_url: 'http://dataservice.accuweather.com/locations/v1/cities/autocomplete',
-  deafult_weather_url: 'http://dataservice.accuweather.com/currentconditions/v1/',
-  five_dayes_forecast_url: 'http://dataservice.accuweather.com/forecasts/v1/daily/5day/',
+  auto_complete_url: 'https://dataservice.accuweather.com/locations/v1/cities/autocomplete',
+  deafult_weather_url: 'https://dataservice.accuweather.com/currentconditions/v1/',
+  five_dayes_forecast_url: 'https://dataservice.accuweather.com/forecasts/v1/daily/5day/',
   weather_icons_url: 'https://www.accuweather.com/images/weathericons/'
 };
